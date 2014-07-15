a4eedba0a6ff1859bcbc1a4a0a881a6e7396edf6
R. Sharma <rahul2.sharma@Mac132.local>
2014-07-15 14:43:45 +0530

    Commit on New Branch

---
+++ GitUserExample/MapFeature/MapFeature.h
@@ -0,0 +1,13 @@
+//
+//  MapFeature.h
+//  GitUserExample
+//
+//  Created by R. Sharma on 7/15/14.
+//  Copyright (c) 2014 AgileMobileDev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapFeature : NSObject
+
+@end
---
+++ GitUserExample/MapFeature/MapFeature.m
@@ -0,0 +1,13 @@
+//
+//  MapFeature.m
+//  GitUserExample
+//
+//  Created by R. Sharma on 7/15/14.
+//  Copyright (c) 2014 AgileMobileDev. All rights reserved.
+//
+
+#import "MapFeature.h"
+
+@implementation MapFeature
+
+@end
